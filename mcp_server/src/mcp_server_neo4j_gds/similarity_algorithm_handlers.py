4fb5522a5c1fffddbc884c012e56234bdb6d422a
ioannispan <ioannis.panagiotas@neotechnology.com>
2025-09-23 14:25:20 +0200

    Support in similarity handlers

--- mcp_server/src/mcp_server_neo4j_gds/similarity_algorithm_handlers.py
+++ mcp_server/src/mcp_server_neo4j_gds/similarity_algorithm_handlers.py
@@ -1,8 +1,8 @@
 import logging
 from typing import Dict, Any
 
-from .algorithm_handler import AlgorithmHandler
-from .gds import projected_graph
+from .algorithm_handler import AlgorithmHandler, clean_params
+from .gds import projected_graph_from_params
 from .node_translator import (
     translate_ids_to_identifiers,
     translate_identifiers_to_ids,
@@ -13,18 +13,17 @@ logger = logging.getLogger("mcp_server_neo4j_gds")
 
 class NodeSimilarityHandler(AlgorithmHandler):
     def node_similarity(self, **kwargs):
-        with projected_graph(self.gds) as G:
-            params = {
-                k: v
-                for k, v in kwargs.items()
-                if v is not None
-                and k
-                not in [
+        with projected_graph_from_params(self.gds, kwargs=kwargs) as G:
+            gds_params = clean_params(
+                kwargs,
+                [
+                    "nodeLabels",
+                    "relTypes",
                     "nodeIdentifierProperty",
                     "sourceNodeFilter",
                     "targetNodeFilter",
-                ]
-            }
+                ],
+            )
             node_identifier_property = kwargs.get("nodeIdentifierProperty")
             source_nodes = kwargs.get("sourceNodeFilter", None)
             target_nodes = kwargs.get("targetNodeFilter", None)
@@ -33,18 +32,18 @@ class NodeSimilarityHandler(AlgorithmHandler):
                 source_nodes,
                 "sourceNodeFilter",
                 node_identifier_property,
-                params,
+                gds_params,
             )
             translate_identifiers_to_ids(
                 self.gds,
                 target_nodes,
                 "targetNodeFilter",
                 node_identifier_property,
-                params,
+                gds_params,
             )
-            logger.info(f"Node Similarity parameters: {params}")
+            logger.info(f"Node Similarity parameters: {gds_params}")
             node_similarity_result = self.gds.nodeSimilarity.filtered.stream(
-                G, **params
+                G, **gds_params
             )
 
         # Add node names to the results if nodeIdentifierProperty is provided
@@ -80,23 +79,24 @@ class NodeSimilarityHandler(AlgorithmHandler):
             relationshipWeightProperty=arguments.get("relationshipWeightProperty"),
             similarityMetric=arguments.get("similarityMetric"),
             useComponents=arguments.get("useComponents"),
+            nodeLabels=arguments.get("nodeLabels"),
+            relTypes=arguments.get("relTypes"),
         )
 
 
 class KNearestNeighborsHandler(AlgorithmHandler):
     def k_nearest_neighbors(self, **kwargs):
-        with projected_graph(self.gds) as G:
-            params = {
-                k: v
-                for k, v in kwargs.items()
-                if v is not None
-                and k
-                not in [
+        with projected_graph_from_params(self.gds, kwargs=kwargs) as G:
+            gds_params = clean_params(
+                kwargs,
+                [
+                    "nodeLabels",
+                    "relTypes",
                     "nodeIdentifierProperty",
                     "sourceNodeFilter",
                     "targetNodeFilter",
-                ]
-            }
+                ],
+            )
             node_identifier_property = kwargs.get("nodeIdentifierProperty")
             source_nodes = kwargs.get("sourceNodeFilter", None)
             target_nodes = kwargs.get("targetNodeFilter", None)
@@ -105,18 +105,18 @@ class KNearestNeighborsHandler(AlgorithmHandler):
                 source_nodes,
                 "sourceNodeFilter",
                 node_identifier_property,
-                params,
+                gds_params,
             )
             translate_identifiers_to_ids(
                 self.gds,
                 target_nodes,
                 "targetNodeFilter",
                 node_identifier_property,
-                params,
+                gds_params,
             )
 
             logger.info(f"K-Nearest Neighbors parameters: {kwargs}")
-            k_nearest_neighbors_result = self.gds.knn.filtered.stream(G, **params)
+            k_nearest_neighbors_result = self.gds.knn.filtered.stream(G, **gds_params)
 
         # Add node names to the results if nodeIdentifierProperty is provided
         node_identifier_property = kwargs.get("nodeIdentifierProperty")
@@ -152,4 +152,6 @@ class KNearestNeighborsHandler(AlgorithmHandler):
             similarityCutoff=arguments.get("similarityCutoff"),
             perturbationRate=arguments.get("perturbationRate"),
             seedTargetNodes=arguments.get("seedTargetNodes"),
+            nodeLabels=arguments.get("nodeLabels"),
+            relTypes=arguments.get("relTypes"),
         )
